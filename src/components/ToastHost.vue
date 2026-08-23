c6de469686ee5f39a47163d6900e659547d44d83
py <py@pc.com>
2026-08-23 14:46:35 +0800

    fix: 修复 toast z-index 过低

--- src/components/ToastHost.vue
+++ src/components/ToastHost.vue
@@ -7,7 +7,7 @@ const { toasts } = useToast();
 <template>
   <Teleport to="body">
     <div
-      class="pointer-events-none fixed inset-x-0 bottom-6 z-50 flex flex-col items-center gap-2"
+      class="pointer-events-none fixed inset-x-0 bottom-6 z-[100] flex flex-col items-center gap-2"
     >
       <TransitionGroup
         enter-active-class="transition duration-200 ease-out"
